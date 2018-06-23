6f4947ef0cb2543f7d36b4a2569466438a07ae12
Daniel Wilkes <daniel_wilkes@outlook.com>
2018-06-23 18:53:59 +0100

    Code refactoring and commenting

--- ViewModels/BaseViewModel.cs
+++ ViewModels/BaseViewModel.cs
@@ -6,7 +6,9 @@ using System.Threading.Tasks;
 
 namespace CountdownTimer.ViewModels
 {
+    //Class used as container for all View models
     public class BaseViewModel : ObservableObject
     {
+        //Only Logic pertaining to every VM should reside here. 
     }
 }
--- ViewModels/CountdownSettingsViewModel.cs
+++ ViewModels/CountdownSettingsViewModel.cs
@@ -11,7 +11,7 @@ namespace CountdownTimer.ViewModels
 
         public CountdownSettingsViewModel()
         {
-            Console.WriteLine("abc");
+            
         }
     }
 }
